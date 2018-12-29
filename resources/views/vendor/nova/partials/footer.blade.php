e86ec9bb9f778e1913f73e3d007be6e62017c6eb
Loc Nguyen <binbinvn@gmail.com>
2018-12-29 18:36:31 +0000

    nova published

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://japan-tour.songhantourist.com" class="text-primary dim no-underline">Kilala Japan Tour</a>
+    <a href="http://nova.laravel.com" class="text-primary dim no-underline">Kilala Tour Japan</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} - By Break Field Vietnam
+    &copy; {{ date('Y') }} Song Han Tourist - By Break Field Vietnam
     <span class="px-1">&middot;</span>
     v{{ Laravel\Nova\Nova::version() }}
 </p>
